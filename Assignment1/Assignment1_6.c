a75f384ad1bc043410d9440b0e56de701c6a6928
Meetsiqol997 <meet@siqol.com>
2026-07-07 11:23:52 +0530

    Question6 : Program of generates the multiplication Table

---
+++ Assignment1/Assignment1_6.c
@@ -0,0 +1,20 @@
+/*Write a program to accept a number and generate its multiplication table 
+up to 10*/
+#include<stdio.h>
+// int return type , main is function name, () is parameter
+int main(void)
+{
+    int num; // Variable to store the number 
+
+    printf("Enter a num: "); //  enter a number
+    scanf("%d", &num); // Accept input 
+
+    // Loop to print table from 1 to 10
+    for(int i = 1; i <= 10; i++)
+    {
+        int Table = num * i; // Calculate multiplication value
+        printf("%d\n", num*i); // Display table values
+    }
+
+    return 0; // return a integer value (0 means no error)
+}
