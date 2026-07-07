f19d0d85450c8fe90cbfeeb35822d2dd27491af9
Meetsiqol997 <meet@siqol.com>
2026-07-07 10:56:36 +0530

    Question1 : Program of Hello world

---
+++ Assignment1/Assignment1_1.c
@@ -0,0 +1,9 @@
+/*Write a program to print "Hello, World!" on the screen.*/
+#include<stdio.h>
+// int return type , main is function name, () is parameter
+int main()
+{
+    printf("Hello world \n"); // output printing the hello world
+    return 0; // return a integer value (0 means no error)
+    
+}
